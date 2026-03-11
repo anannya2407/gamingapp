48ae6761fad534a3bf312482f6110cb948036a7a
anannya_2407 <anannyasenapati2007@gmail.com>
2026-03-11 13:53:48 +0530

    main

---
+++ demo.java
@@ -0,0 +1,14 @@
+
+
+
+class Array1{
+    int roll[][]= {{1,2,3},{4,5,6}};
+	
+}
+public class Arrayex1{
+public static void main(String[] args){
+    Array1 ob=new Array1();
+    System.out.println("Multi dimensional" + ob.roll[0][1]);
+	
+}
+}
