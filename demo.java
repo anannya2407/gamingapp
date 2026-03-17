8fb2b7d3ab694bacae3df8ddb9ef87ba703e490e
anannya_2407 <anannyasenapati2007@gmail.com>
2026-03-17 13:38:12 +0530

    main

--- demo.java
+++ demo.java
@@ -1,14 +1,14 @@
+public class HelloApp {
 
+    public static void main(String[] args) {
 
-
-class Array1{
-    int roll[][]= {{1,2,3},{4,5,6}};
-	
-}
-public class Arrayex1{
-public static void main(String[] args){
-    Array1 ob=new Array1();
-    System.out.println("Multi dimensional" + ob.roll[0][1]);
-	
-}
+        // Check if at least one argument is provided
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Default message if no argument is passed
+            System.out.println("Hello, World!");
+        }
+    }
 }
